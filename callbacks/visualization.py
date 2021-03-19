cdca0681fd718a8678bd94693df79abbd3658624
niniack <nishantaswani@nyu.edu>
2021-03-19 16:25:05 +0400

    Fix circular import

--- callbacks/visualization.py
+++ callbacks/visualization.py
@@ -2,7 +2,8 @@ import numpy as np
 from server import app
 from dash.dependencies import Input, Output
 from data.data import build_sunburst, update_sunburst
-from app import fig
+
+fig = build_sunburst()
 
 
 @app.callback(
